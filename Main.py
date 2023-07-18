136f53cee8ff316a445ee79dd667557fa4e39c89
Derek9745 <31524195+Derek9745@users.noreply.github.com>
2023-07-18 17:59:43 -0400

    startup screen

--- Main.py
+++ Main.py
@@ -10,6 +10,10 @@ PLAYER_WIDTH = 40
 PLAYER_HEIGHT = 40;
 PLAYER_VEL =5;
 
+
+
+
+
 def draw(player):
     WIN.blit(BG,(0,0))
 
@@ -19,18 +23,29 @@ def draw(player):
 
 
 
+
 def main():
     run = True
-    player = pygame.Rect(200,HEIGHT-PLAYER_HEIGHT, PLAYER_WIDTH,PLAYER_HEIGHT)
+    player = pygame.Rect(200,0, PLAYER_WIDTH,PLAYER_HEIGHT)
+    stoneblock = pygame.Rect(100, 100,PLAYER_WIDTH,PLAYER_HEIGHT)
+    clock = pygame.time.Clock()
     while run:
+        clock.tick(60)
+
+        if player.y + player.height <= HEIGHT:
+            player.y+=1
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
                 break
 
         keys = pygame.key.get_pressed()
-        if keys[pygame.K_LEFT]:
+        if keys[pygame.K_LEFT] and player.x - PLAYER_VEL >=0:
             player.x -= PLAYER_VEL
+        if keys[pygame.K_RIGHT]and player.x + PLAYER_VEL + player.width <= WIDTH:
+            player.x += PLAYER_VEL
+        if keys[pygame.K_DOWN] and player.y + PLAYER_VEL + player.height <= HEIGHT:
+            player.y += PLAYER_VEL
         draw(player)
 
     pygame.quit()
