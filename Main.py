4420516001c0b95078a67f34fec85ba93853de39
Derek9745 <31524195+Derek9745@users.noreply.github.com>
2023-10-26 12:20:39 -0400

    startup screen

--- Main.py
+++ Main.py
@@ -1,54 +1,53 @@
 import pygame
 import time
 import random
+import sys
 
-WIDTH,HEIGHT = 850, 850
+WIDTH,HEIGHT = 800, 800
 WIN = pygame.display.set_mode((WIDTH,HEIGHT))
-pygame.display.set_caption("Snake Blocks")
-BG = pygame.transform.scale(pygame.image.load("Python wallpaper.jpg"), (WIDTH,HEIGHT))
-PLAYER_WIDTH = 40
-PLAYER_HEIGHT = 40;
-PLAYER_VEL =5;
+pygame.display.set_caption("Tetris")
+clock = pygame.time.Clock()
 
+PLAYER_VEL =5
+FPS = 60
 
+class Grid:
+    def draw_grid(self):
+        self.num_rows = 20
+        self.num_cols = 10
+        self.cell_size = 30
+        self.grid = ([[0 for j in range(self.num_cols)] for i in range(self.num_rows)])
+        self.colors = self.get_cell_colors()
+    def print_grid(self):
+        for row in range(self.num_rows):
+            for column in range(self.num_cols):
+                print(self.grid[row][column], end = " ")
+            print()
 
+    def get_cell_colors(self):
+        dark_grey = (26,31,40)
+        green = (47,230,23)
+        red = (232,18,18)
+        orange = (226,116,17)
+        yellow = (237,234,4)
+        purple = (116,0,247)
+        cyan = (21,204,209)
+        blue = (13,64,216)
 
+        return [dark_grey,green,red,orange,yellow, purple,cyan,blue]
 
-def draw(player):
-    WIN.blit(BG,(0,0))
-
-    pygame.draw.rect(WIN, "blue", player)
-
-    pygame.display.update()
-
-
-
+    def draw(self):
 
 def main():
     run = True
-    player = pygame.Rect(200,0, PLAYER_WIDTH,PLAYER_HEIGHT)
-    stoneblock = pygame.Rect(100, 100,PLAYER_WIDTH,PLAYER_HEIGHT)
-    clock = pygame.time.Clock()
     while run:
-        clock.tick(60)
-
-        if player.y + player.height <= HEIGHT:
-            player.y+=1
+        clock.tick(FPS)
+        pygame.display.update
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                run = False
-                break
+                pygame.quit()
+                sys.exit()
 
-        keys = pygame.key.get_pressed()
-        if keys[pygame.K_LEFT] and player.x - PLAYER_VEL >=0:
-            player.x -= PLAYER_VEL
-        if keys[pygame.K_RIGHT]and player.x + PLAYER_VEL + player.width <= WIDTH:
-            player.x += PLAYER_VEL
-        if keys[pygame.K_DOWN] and player.y + PLAYER_VEL + player.height <= HEIGHT:
-            player.y += PLAYER_VEL
-        draw(player)
-
-    pygame.quit()
 
 if __name__== "__main__":
     main()
